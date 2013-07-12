14240e370ecfd3ba4db62231af643bfa7d2873de
Sheniff <sheniff@gmail.com>
2013-07-11 20:56:23 -0700

    Fix fake tests and gitignore

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('codeheroWebappApp'));
+  beforeEach(module('codehero'));
 
   var MainCtrl,
     scope;
@@ -11,12 +11,12 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('ChallengeCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
 });
